473cbc75b8a9883c36c33992c852f6158e7b31ed
Noah <sovannarasay11@gmail.com>
2024-06-16 10:45:07 -0600

    Adds media element NuGet packages and example control

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace NASA_APOD_TestDemo
 {
@@ -9,6 +10,7 @@ namespace NASA_APOD_TestDemo
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkitMediaElement()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
